caeba7aacc97016516897be267574cc777e23fb2
SkyNetServer <malikasinger1@gmail.com>
2015-09-16 21:42:51 -0800

    started draw page making

--- views/draw/draw.js
+++ views/draw/draw.js
@@ -1,3 +1,24 @@
 /**
  * Created by SkyNetServer on 9/14/2015.
  */
+angular.module("drawApp")
+
+.controller("drawController",function($scope,$firebaseArray, $mdToast){
+    $scope.password = "";
+    $scope.hardCodedPassword = "pakistanpakistan";
+    $scope.showPasswordForm = true;
+    $scope.showPageContent = false;
+    $scope.errorText= '';
+
+
+    $scope.checkPassword = function(){
+        if($scope.password == $scope.hardCodedPassword){
+            $scope.showPasswordForm = false;
+            $scope.showPageContent = true;
+        }else{
+            $scope.errorText = 'You have entered wrong password';
+
+        }
+    }
+
+});
